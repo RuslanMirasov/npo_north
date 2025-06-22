5ff46208b9b00f0ca84e5d20db8549e8faa89c45
Ruslan <info@mirasov.dev>
2025-06-22 23:04:51 +0200

    smp page

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -3,21 +3,31 @@ export const initSliders = () => {
   if (!sliders.length) return;
 
   sliders.forEach(slider => {
-    const prevBtn = slider.querySelector('[data-prev]');
+    const slidesPerView = slider.dataset.slidesPerView || 1;
+    const spaceBetween = slider.dataset.spaceBetween || 0;
+    const spaceBetweenMobile = slider.dataset.spaceBetweenMobile || 0;
     const nextBtn = slider.querySelector('[data-next]');
+    const prevBtn = slider.querySelector('[data-prev]');
 
     new Swiper(slider, {
       speed: 1000,
-      allowTouchMove: false,
-      simulateTouch: false,
       autoplay: {
         delay: 5000,
-        disableOnInteraction: false,
+        disableOnInteraction: true,
       },
-      loop: true,
       navigation: {
         nextEl: nextBtn,
         prevEl: prevBtn,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+          spaceBetween: spaceBetweenMobile,
+        },
+        768: {
+          slidesPerView,
+          spaceBetween,
+        },
       },
     });
   });
